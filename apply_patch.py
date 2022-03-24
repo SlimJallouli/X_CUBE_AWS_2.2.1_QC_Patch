da732b1e6d7601161ec835d33fd69138d1e63723
Slim Jallouli <41168224+Slim-ST@users.noreply.github.com>
2022-03-24 16:38:00 -0700

    Add license to apply_patch.py

--- apply_patch.py
+++ apply_patch.py
@@ -1,3 +1,18 @@
+#******************************************************************************
+# * @file           : apply_patch.py
+# * @brief          : Automatically registers new thing to AWS associated with connected STM32.
+# ******************************************************************************
+# * @attention
+# *
+# * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+# * All rights reserved.</center></h2>
+# *
+# * This software component is licensed by ST under Ultimate Liberty license
+# * SLA0044, the "License"; You may not use this file except in compliance with
+# * the License. You may obtain a copy of the License at:
+# *                             www.st.com/SLA0044
+# *
+# ******************************************************************************
 import shutil, os
 
 del_dirPath_list = ['../Projects/NUCLEO-H755ZI',
